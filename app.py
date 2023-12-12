928287310213c7a11eed9fa48490771ff99c0bfd
Eddy Ejembi <eddyejembi2018@gmail.com>
2023-12-12 22:00:21 +0100

    update

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
 import requests
 import streamlit as st
 from PIL import Image
-import pyperclip
+import threading
 import time
 
 
 def send_image_and_prompt(image, prompt):
-    """
-    Sends an image and additional prompt to the server API.
-
-    Args:
-        image_path: Path to the image file.
-        prompt: Additional text prompt.
-
-    Returns:
-        The response from the server API.
-    """
 
     # Prepare the request
     url = "https://generative-image-caption.vercel.app/analyze"
@@ -27,45 +17,76 @@ def send_image_and_prompt(image, prompt):
 
     # Check for errors
     if response.status_code != 200:
-        raise Exception(f"Error: {response.status_code} - {response.text}")
+        #raise Exception(f"Error: {response.status_code} - {response.text}")
+        st.info(f"Error: ERROR GENERATING CAPTION... PLEASE TRY AGAIN.")
 
     # Return the response
     return response.json()
 
 
 def main():
-    st.title("Image Processing App")
+    st.title("Social Media Image Caption Generator")
+    st.subheader("Generate exciting and engaging social media captions for your images")
 
+    #message = []
     # Allow user to upload image
     uploaded_file = st.file_uploader("Choose an image:", type=["png", "jpg", "jpeg"])
 
+
     # Show prompt field only if image is uploaded
     if uploaded_file:
-        prompt = st.text_input("Enter additional prompt (optional):")
+        # Display the uploaded image
+        st.image(uploaded_file, caption="Uploaded Image", width=300)
+
+        prompt = st.text_input("Enter an additional prompt (optional):")
+
+        # Get the current session state
+        session_state = st.session_state
+
+        # Display button and handle button click logic
+        if "last_click_time" not in session_state:
+            session_state.last_click_time = 0
+
+        elapsed_time = time.time() - session_state.last_click_time
         
+        # Generate caption button
         if st.button("Generate Caption"):
-                
-            # Disable button and display spinner while processing
-            with st.spinner("Generating caption..."):
-                response = send_image_and_prompt(uploaded_file, prompt)
+            if elapsed_time > 60:
+                    
+                # Disable button and display spinner while processing
+                with st.spinner("Generating caption..."):
+                    session_state.last_click_time = time.time()  # Update last click time
+                    response = send_image_and_prompt(uploaded_file, prompt)
 
-            # Display the response
-            if "error" in response:
-                #st.error(response["error"])
-                st.error("ERROR GENERATING CAPTION")
+                    # Store the original captions in session state
+                    session_state.original_captions = response.get("response", "").splitlines()
+
+                # Display the response
+                if "error" in response:
+                    #st.error(response["error"])
+                    st.error("ERROR GENERATING CAPTION")
+                else:
+                    # Split the response into individual captions
+                    st.info("Please wait 1 minute before generating another caption.")
+                    captions = response["response"].splitlines()
+                    print(type(captions))
+
+                    # Display each caption as a separate entity
+                    for caption in captions:
+                        display = caption
+                        print(display)
+                        st.write(display)
+                        # Add a copy caption
+                        #st.code(display, language="python")
+                    
             else:
-                # Split the response into individual captions
-                st.caption("Please wait 1 minute before generating another caption.")
-                captions = response["response"].splitlines()
-
-                # Display each caption as a separate entity
-                for caption in captions:
-                    display = caption
-                    print(display)
-                    st.write(display)
-                    # Add a copy caption
-                    #st.code(display, language="python")
-
+                st.info(f"Please wait {round(60 - elapsed_time)} seconds before generating another caption.")
+                #captions = response["response"].splitlines()
+                st.caption("Previous Captions:")
+                original_captions = st.session_state.get("original_captions", [])
+                for caption in original_captions:
+                        display = caption
+                        st.write(display)
     else:
         st.info("Please upload an image to begin.")
 
